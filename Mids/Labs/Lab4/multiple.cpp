80d59b058c7b682be3876a75be68ab6e1d0ca498
Humna Imran <168326829+hamnasz@users.noreply.github.com>
2024-09-21 15:09:45 +0500

    Hamna Mirza

---
+++ Mids/Labs/Lab4/multiple.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+int main()
+{
+	int x, y;
+	cout << "Enter 1st num: ";
+	cin >> x;
+	cout << "Enter 2nd num: ";
+	cin >> y;
+	if (x % y == 0 || y % x == 0)
+		cout << "Second number " << y << " is multiple of " << x;
+}

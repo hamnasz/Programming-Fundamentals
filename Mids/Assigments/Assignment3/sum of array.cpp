68e4c65bc6cc35dbc6e45f1bd8d5f79fed2b4507
Humna Imran <168326829+hamnasz@users.noreply.github.com>
2024-09-21 15:43:04 +0500

    Hamna Mirza

---
+++ Mids/Assigments/Assignment3/sum of array.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n, sum = 0;
+
+    cout << "Enter size: ";
+    cin >> n;
+
+    int arr[n];
+
+    cout << "Enter elements: ";
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        sum = sum + arr[i];
+    }
+    cout << "Sum of array is: " << sum << endl;
+}

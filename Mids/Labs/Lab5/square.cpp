63e1112ccb50258134526bda936078d5c71c7fd7
Humna Imran <168326829+hamnasz@users.noreply.github.com>
2024-09-21 15:11:20 +0500

    Hamna Mirza

---
+++ Mids/Labs/Lab5/square.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+int main()
+{
+	int x, y;
+	cout << "Enter 1st num: ";
+	cin >> x;
+	cout << "Enter 2nd num: ";
+	cin >> y;
+	if (x * x == y)
+		cout << "Second number " << y << " is square of " << x;
+}
